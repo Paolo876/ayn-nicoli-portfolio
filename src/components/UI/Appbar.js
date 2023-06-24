8e8d0877587e2032a31ac8faf025ed60f522e89b
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-24 13:56:40 -0700

    added logos

--- src/components/UI/Appbar.js
+++ src/components/UI/Appbar.js
@@ -22,39 +22,26 @@ const Appbar = () => {
     <AppBar component="nav" position="fixed" sx={{minHeight: 0, backgroundColor: "background.default", boxShadow: 5}}>
       <Container maxWidth="xl">
         <Grid container sx={{py:{xs:.5, sm:1.15, md: 1.2, lg: .75 }}}>
-          <Grid item xs={4}>
-            <Link component={ReactLink} to="/" >
-              {/* <Box>
-                <Typography variant="body1" sx={{letterSpacing: -.25}} fontWeight={700} fontSize={{lg: 20}} lineHeight={1} color="primary.dark">AYN NICOLI LAQUINDANUM</Typography>
-                <Box sx={{display: "flex", alignItems: "center", mx: "auto", width: "100%", justifyContent: "center", mt: .25}} >
-                  <Typography variant="body1" lineHeight={1} letterSpacing={.25} fontWeight={300} fontSize={10.5} color="primary.dark">LICENSED ARCHITECT</Typography>
-                  <Typography variant="body1" color="primary.dark" mx={1} fontSize={11} lineHeight={1} fontWeight={400}>|</Typography>
-                  <Typography variant="body1" lineHeight={1} letterSpacing={.25} fontWeight={300} fontSize={10.5} color="primary.dark">GRAPHIC ARTIST</Typography>
-                </Box>
-              </Box> */}
-            </Link>
+          <Grid item xs={4} sx={{display: "flex", justifyContent: "space-between", alignItems: "center"}}>
+            <Button size="medium"><Typography color="primary.dark" variant="h6" sx={{fontWeight: 700, lineHeight: 1, fontSize: 20, letterSpacing: -.5}}>ARCHITECTURAL <Box component="span" fontWeight={400}>DESIGN</Box></Typography></Button>
+            <Box sx={{height: 6, width: 6, borderRadius: "50%", backgroundColor: "text.disabled"}}></Box>
+            <Button size="medium"><Typography color="primary.dark" variant="h6" sx={{fontWeight: 700, lineHeight: 1, fontSize: 20, letterSpacing: -.5}}>GRAPHIC <Box component="span" fontWeight={400}>DESIGN</Box></Typography></Button>  
           </Grid>
           <Grid 
             item 
-            xs={8}              
+            // xs={8}              
             sx={{
               ml: { xs:0, md:"auto" }, 
               display:{ xs: "none", md: "flex" }, 
               flexDirection: "row", 
               gap: { md:4, lg: 5,xl: 7.5 },
-              alignItems: "flex-end",
+              alignItems: "center",
               justifyContent: "right"
             }}
           >
             <Link component={NavLink} to="/about" draggable={false}><Typography color="primary.dark" sx={linkStyles}>ABOUT</Typography></Link>
             <Link component={NavLink} to="/contact" draggable={false}><Typography color="primary.dark" sx={linkStyles}>CONTACT</Typography></Link>
           </Grid>
-        </Grid>
-        <Grid container sx={{pb: .25}}>
-          <Grid item xs={4} sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-          <Button size="small"><Typography color="primary.dark" variant="body2" >ARCHITECTURAL PROJECTS</Typography></Button>
-          <Button size="small"><Typography color="primary.dark" variant="body2">GRAPHIC DESIGN PROJECTS</Typography></Button>  
-        </Grid>
         </Grid>
       </Container>
     </AppBar>
