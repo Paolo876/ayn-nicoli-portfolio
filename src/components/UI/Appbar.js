7ade02b9623cb876049a7dd589e456aae2c07b2f
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-24 13:45:01 -0700

    updated favicon

--- src/components/UI/Appbar.js
+++ src/components/UI/Appbar.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { NavLink, Link as ReactLink } from 'react-router-dom';
-import { Typography, AppBar, Container, Box, Link, IconButton } from '@mui/material'
+import { Typography, AppBar, Container, Box, Link, IconButton, Grid, Button } from '@mui/material'
 import MenuIcon from '@mui/icons-material/Menu';
 
 const linkStyles = {
@@ -21,28 +21,41 @@ const Appbar = () => {
   return (
     <AppBar component="nav" position="fixed" sx={{minHeight: 0, backgroundColor: "background.default", boxShadow: 5}}>
       <Container maxWidth="xl">
-        <Box sx={{display: "flex", flexDirection: "row", px:{xs:0, sm: .5, md:1.5, lg: 1.5, xl: 3 }, py:{xs:.5, sm:1.15, md: 1.2, lg: 1.25 }, alignItems: "center"}}>
-          <Link component={ReactLink} to="/" >
-            <Typography variant="body1" sx={{letterSpacing: -.5}} fontWeight={700} fontSize={{lg: 23}} lineHeight={1} color="primary.dark">AYN NICOLI LAQUINDANUM</Typography>
-            <Box sx={{display: "flex", alignItems: "center", mx: "auto", width: "100%", justifyContent: "center", mt: .25}} >
-              <Typography variant="body1" lineHeight={1} letterSpacing={.25} fontWeight={300} fontSize={12} color="primary.dark">LICENSED ARCHITECT</Typography>
-              <Typography variant="body1" color="primary.dark" mx={1} fontSize={16} lineHeight={1} fontWeight={400}>|</Typography>
-              <Typography variant="body1" lineHeight={1} letterSpacing={.25} fontWeight={300} fontSize={12} color="primary.dark">GRAPHIC ARTIST</Typography>
-            </Box>
-          </Link>
-          <Box 
+        <Grid container sx={{py:{xs:.5, sm:1.15, md: 1.2, lg: .75 }}}>
+          <Grid item xs={4}>
+            <Link component={ReactLink} to="/" >
+              {/* <Box>
+                <Typography variant="body1" sx={{letterSpacing: -.25}} fontWeight={700} fontSize={{lg: 20}} lineHeight={1} color="primary.dark">AYN NICOLI LAQUINDANUM</Typography>
+                <Box sx={{display: "flex", alignItems: "center", mx: "auto", width: "100%", justifyContent: "center", mt: .25}} >
+                  <Typography variant="body1" lineHeight={1} letterSpacing={.25} fontWeight={300} fontSize={10.5} color="primary.dark">LICENSED ARCHITECT</Typography>
+                  <Typography variant="body1" color="primary.dark" mx={1} fontSize={11} lineHeight={1} fontWeight={400}>|</Typography>
+                  <Typography variant="body1" lineHeight={1} letterSpacing={.25} fontWeight={300} fontSize={10.5} color="primary.dark">GRAPHIC ARTIST</Typography>
+                </Box>
+              </Box> */}
+            </Link>
+          </Grid>
+          <Grid 
+            item 
+            xs={8}              
             sx={{
               ml: { xs:0, md:"auto" }, 
               display:{ xs: "none", md: "flex" }, 
               flexDirection: "row", 
               gap: { md:4, lg: 5,xl: 7.5 },
               alignItems: "flex-end",
+              justifyContent: "right"
             }}
-            >
+          >
             <Link component={NavLink} to="/about" draggable={false}><Typography color="primary.dark" sx={linkStyles}>ABOUT</Typography></Link>
             <Link component={NavLink} to="/contact" draggable={false}><Typography color="primary.dark" sx={linkStyles}>CONTACT</Typography></Link>
-          </Box>
-        </Box>
+          </Grid>
+        </Grid>
+        <Grid container sx={{pb: .25}}>
+          <Grid item xs={4} sx={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+          <Button size="small"><Typography color="primary.dark" variant="body2" >ARCHITECTURAL PROJECTS</Typography></Button>
+          <Button size="small"><Typography color="primary.dark" variant="body2">GRAPHIC DESIGN PROJECTS</Typography></Button>  
+        </Grid>
+        </Grid>
       </Container>
     </AppBar>
   )
--- src/pages/Home/ArchitecturalWorks.jsx
+++ src/pages/Home/ArchitecturalWorks.jsx
@@ -8,8 +8,8 @@ const ArchitecturalWorks = () => {
         <Grid container sx={{height: "100%"}}>
           <Grid item xs={4} sx={{zIndex: 2, backgroundColor: "primary.main", display: "flex", flexDirection: "column", height: "100%", justifyContent: "center", p: 2, pb: 10}}>
             <Box align="left" sx={{}}>
-              <Typography variant="h2" lineHeight={1.2} fontWeight={700} sx={{opacity: .95}} fontSize={{lg: 54}} color="background.default" ml={.35}>ARCHITECTURAL</Typography>
-              <Typography variant="h3" fontWeight={200} lineHeight={1.2} sx={{opacity: .8}} letterSpacing={1} color="background.paper">DESIGN</Typography>
+              <Typography variant="h2" lineHeight={1.1} fontWeight={700} sx={{opacity: .95}} fontSize={{lg: 54}} color="background.default" ml={.35}>ARCHITECTURAL</Typography>
+              <Typography variant="h3" fontWeight={200} lineHeight={1.1} sx={{opacity: .8}} letterSpacing={1} color="background.paper">DESIGN</Typography>
             </Box>
             <Box align="left" sx={{mt: 5}}>
               <Typography variant="body1" letterSpacing={4.5} fontWeight={100} sx={{opacity: .7}} lineHeight={1.6} fontSize={23} color="background.default">3D MODELING</Typography>
