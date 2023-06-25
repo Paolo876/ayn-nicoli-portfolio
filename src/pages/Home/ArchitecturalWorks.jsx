26abd09222681f2b2af3b879f7fd09eacabebf1b
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-25 15:28:30 -0700

    updated architecturalWorks component

--- src/pages/Home/ArchitecturalWorks.jsx
+++ src/pages/Home/ArchitecturalWorks.jsx
@@ -27,25 +27,56 @@ const ArchitecturalWorks = ({ setShowModal }) => {
           <Grid container sx={{height: "100%"}}>
             <Slide direction="down" in={true} mountOnEnter unmountOnExit timeout={800} >
               <Grid item xs={3.2} sx={{zIndex: 2, backgroundColor: "primary.main", display: "flex", flexDirection: "column", justifyContent: "center", p: 2, mb: 5, pb: 20, boxShadow: 2}}>
-                <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "700ms" }}>
+                <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "700ms" }} mountOnEnter>
                   <ButtonBase 
+                    disableRipple
                     onClick={() => handleClick("all")}
-                    align="left" 
-                    sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent: "left"}}
+                    
+                    sx={{ display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent: "left", }}
                   >
-                    <Typography variant="h2" lineHeight={1.1} fontWeight={700} sx={{opacity: .95}} fontSize={{lg: 42}} color="background.default" ml={.25}>ARCHITECTURAL</Typography>
-                    <Typography variant="h3" fontWeight={200} lineHeight={1.1} sx={{opacity: .8}} fontSize={{lg: 42}} letterSpacing={1} color="background.paper">DESIGN</Typography>
+                    <Box sx={{ transition: "all 250ms ease 50ms", transform: activeProjects && activeProjects.id === "all" ? "scale(1.025)" : "scale(1)" }} align="left" >
+                      <Typography variant="h2" lineHeight={1.1} fontWeight={700} sx={{opacity: .95}} fontSize={{lg: 42}} color="background.default" ml={.25}>ARCHITECTURAL</Typography>
+                      <Typography variant="h3" fontWeight={200} lineHeight={1.1} sx={{opacity: .8}} fontSize={{lg: 42}} letterSpacing={1} color="background.paper">DESIGN</Typography>
+                    </Box>
                   </ButtonBase>
                 </Fade>
                 <Box align="left" sx={{mt: 5, display: "flex", flexDirection: "column", textAlign:"left", alignItems: "flex-start"}}>
                   <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1200ms" }}>
-                    <ButtonBase onClick={() => handleClick("interior")}>
-                      <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">INTERIOR VISUALIZATIONS</Typography>
+                    <ButtonBase disableRipple onClick={() => handleClick("interior")} sx={{ml: .5}}>
+                      <Typography 
+                        variant="body1" 
+                        letterSpacing={3} 
+                        sx={{
+                          transition: "all 250ms ease 25ms", 
+                          fontWeight: activeProjects && activeProjects.id === "interior" ? 400 : 200,
+                          opacity: activeProjects && activeProjects.id === "interior" ? .95 : .7, 
+                          transform: activeProjects && activeProjects.id === "interior" ? "scale(1.03)" : "scale(1)" 
+                        }} 
+                        lineHeight={1.8} 
+                        fontSize={18} 
+                        color="background.default"
+                      >
+                        INTERIOR VISUALIZATIONS
+                      </Typography>
                     </ButtonBase>
                   </Fade>
                   <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1500ms" }}>
-                    <ButtonBase onClick={() => handleClick("exterior")}>
-                      <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">EXTERIOR VISUALIZATIONS</Typography>
+                    <ButtonBase disableRipple onClick={() => handleClick("exterior")} sx={{ml: .5}}>
+                      <Typography 
+                        variant="body1" 
+                        letterSpacing={3} 
+                        sx={{
+                          transition: "all 250ms ease 25ms", 
+                          fontWeight: activeProjects && activeProjects.id === "exterior" ? 400 : 200,
+                          opacity: activeProjects && activeProjects.id === "exterior" ? .95 : .7, 
+                          transform: activeProjects && activeProjects.id === "exterior" ? "scale(1.03)" : "scale(1)" 
+                        }} 
+                        lineHeight={1.8} 
+                        fontSize={18} 
+                        color="background.default"
+                      >
+                        EXTERIOR VISUALIZATIONS
+                      </Typography>
                     </ButtonBase>                
                   </Fade>
                 </Box>
