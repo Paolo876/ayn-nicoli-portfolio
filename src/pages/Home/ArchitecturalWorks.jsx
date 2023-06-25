d5c68724d76cd9c998f1c314a6d08ba2990ba4b2
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-24 23:04:04 -0700

    updated home page ui

--- src/pages/Home/ArchitecturalWorks.jsx
+++ src/pages/Home/ArchitecturalWorks.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Container, Grid, Typography, Slide, Fade } from '@mui/material';
+import { Box, Container, Grid, Typography, Slide, Fade, ButtonBase } from '@mui/material';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import Showcase from './Showcase.jsx';
 
@@ -7,37 +7,45 @@ import Showcase from './Showcase.jsx';
 const ArchitecturalWorks = () => {
   const { architecturalProjects } = useAuthContext();
 
-  const projects = [...architecturalProjects.interior, ...architecturalProjects.exterior ];
+  const projects = architecturalProjects ? [...architecturalProjects.interior, ...architecturalProjects.exterior ] : [];
+
 
   return (
-    <Box sx={{position: "relative", height: "100%", backgroundColor: "background.paper"}}>
-      <Container maxWidth="xl" sx={{pt: 0, height: "100%"}}>
-        <Grid container sx={{height: "100%"}}>
-          <Slide direction="down" in={true} mountOnEnter unmountOnExit timeout={800} >
-            <Grid item xs={3.2} sx={{zIndex: 2, backgroundColor: "primary.main", display: "flex", flexDirection: "column", justifyContent: "center", p: 2, mb: 5, pb: 20, boxShadow: 2}}>
-              <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "700ms" }}>
-                <Box align="left">
-                  <Typography variant="h2" lineHeight={1.1} fontWeight={700} sx={{opacity: .95}} fontSize={{lg: 42}} color="background.default" ml={.25}>ARCHITECTURAL</Typography>
-                  <Typography variant="h3" fontWeight={200} lineHeight={1.1} sx={{opacity: .8}} fontSize={{lg: 42}} letterSpacing={1} color="background.paper">DESIGN</Typography>
+    <Fade appear={true} in={true} timeout={{ enter: 1000, exit: 800 }} style={{ transitionDelay: "200ms" }}>
+      <Box sx={{position: "relative", height: "100%", backgroundColor: "background.default"}}>
+        <Container maxWidth="xl" sx={{pt: 0, height: "100%"}}>
+          <Grid container sx={{height: "100%"}}>
+            <Slide direction="down" in={true} mountOnEnter unmountOnExit timeout={800} >
+              <Grid item xs={3.2} sx={{zIndex: 2, backgroundColor: "primary.main", display: "flex", flexDirection: "column", justifyContent: "center", p: 2, mb: 5, pb: 20, boxShadow: 2}}>
+                <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "700ms" }}>
+                  <ButtonBase align="left" sx={{display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent: "left"}}>
+                    <Typography variant="h2" lineHeight={1.1} fontWeight={700} sx={{opacity: .95}} fontSize={{lg: 42}} color="background.default" ml={.25}>ARCHITECTURAL</Typography>
+                    <Typography variant="h3" fontWeight={200} lineHeight={1.1} sx={{opacity: .8}} fontSize={{lg: 42}} letterSpacing={1} color="background.paper">DESIGN</Typography>
+                  </ButtonBase>
+                </Fade>
+                <Box align="left" sx={{mt: 5, display: "flex", flexDirection: "column", textAlign:"left", alignItems: "flex-start"}}>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1200ms" }}>
+                    <ButtonBase>
+                      <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">INTERIOR VISUALIZATIONS</Typography>
+                    </ButtonBase>
+                  </Fade>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1500ms" }}>
+                    <ButtonBase>
+                      <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">EXTERIOR VISUALIZATIONS</Typography>
+                    </ButtonBase>                
+                  </Fade>
                 </Box>
-              </Fade>
-              <Box align="left" sx={{mt: 5}}>
-                <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1200ms" }}>
-                  <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">INTERIOR VISUALIZATIONS</Typography>
-                </Fade>
-                <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1500ms" }}>
-                  <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">EXTERIOR VISUALIZATIONS</Typography>
-                </Fade>
-              </Box>
-            </Grid>
-          </Slide>
-          <Grid item xs={8.8} sx={{zIndex: 2, display: "flex", flexDirection: "column", height: "100%", overflow: "auto", width: "100%"}}>
-            <Showcase projects={projects}/>
+              </Grid>
+            </Slide>
+            <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "2000ms" }}>
+              <Grid item xs={8.8} sx={{zIndex: 2, display: "flex", flexDirection: "column", height: "100%", overflow: "auto", width: "100%"}}>
+                <Showcase projects={projects}/>
+              </Grid>
+            </Fade>
           </Grid>
-        </Grid>
-      </Container>
-
-    </Box>
+        </Container>
+      </Box>
+    </Fade>
   )
 }
 
--- src/pages/Home/GraphicWorks.jsx
+++ src/pages/Home/GraphicWorks.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Container, Grid, Typography } from '@mui/material'
+import { Box, Container, Grid, Typography, Slide, Fade, ButtonBase } from '@mui/material';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import Showcase from './Showcase.jsx';
 
@@ -7,31 +7,56 @@ import Showcase from './Showcase.jsx';
 const GraphicWorks = () => {
   const { graphicProjects } = useAuthContext();
 
-  const projects = [...graphicProjects.illustrations, ...graphicProjects.typographies, ...graphicProjects.print, ...graphicProjects.logo ];
-  console.log(projects)
-  return (
-    <Box sx={{position: "relative", height: "100%", backgroundColor: "background.paper"}}>
-      <Container maxWidth="xl" sx={{pt: 0, height: "100%"}}>
-        <Grid container sx={{height: "100%"}}>
-          <Grid item xs={3.2} sx={{zIndex: 2, backgroundColor: "secondary.main", display: "flex", flexDirection: "column", justifyContent: "center", p: 2, mb: 5, pb: 20, boxShadow: 2}}>
-            <Box align="left" sx={{}}>
-              <Typography variant="h2" lineHeight={1.1} fontWeight={700} sx={{opacity: .95}} fontSize={{lg: 48}} color="background.default">GRAPHIC</Typography>
-              <Typography variant="h3" fontWeight={200} lineHeight={1.1} sx={{opacity: .8}} fontSize={{lg: 48}} letterSpacing={1} color="background.paper">DESIGN</Typography>
-            </Box>
-            <Box align="left" sx={{mt: 5}}>
-              <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">DIGITAL ILLUSTRATIONS</Typography>
-              <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">TYPOGRAPHIES</Typography>
-              <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">LOGO DESIGNS</Typography>
-              <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">PRINT DESIGNS</Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={8.8} sx={{zIndex: 2, display: "flex", flexDirection: "column", height: "100%", overflow: "auto", width: "100%"}}>
-            <Showcase projects={projects}/>
-          </Grid>
-        </Grid>
-      </Container>
+  const projects = graphicProjects ? [...graphicProjects.illustrations, ...graphicProjects.typographies, ...graphicProjects.print, ...graphicProjects.logo ] : [];
 
-    </Box>
+
+  return (
+    <Fade appear={true} in={true} timeout={{ enter: 1000, exit: 800 }} style={{ transitionDelay: "200ms" }}>
+      <Box sx={{position: "relative", height: "100%", backgroundColor: "background.default"}}>
+        <Container maxWidth="xl" sx={{pt: 0, height: "100%"}}>
+          <Grid container sx={{height: "100%"}}>
+            <Slide direction="down" in={true} mountOnEnter unmountOnExit timeout={800} >
+              <Grid item xs={3.2} sx={{zIndex: 2, backgroundColor: "secondary.main", display: "flex", flexDirection: "column", justifyContent: "center", p: 2, mb: 5, pb: 20, boxShadow: 2}}>
+                <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "700ms" }}>
+                  <ButtonBase align="left" sx={{display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent: "left"}}>                <Typography variant="h2" lineHeight={1.1} fontWeight={700} sx={{opacity: .95}} fontSize={{lg: 48}} color="background.default">GRAPHIC</Typography>
+                    <Typography variant="h3" fontWeight={200} lineHeight={1.1} sx={{opacity: .8}} fontSize={{lg: 48}} letterSpacing={1} color="background.paper">DESIGN</Typography>
+                  </ButtonBase>
+                </Fade>
+
+                <Box align="left" sx={{mt: 5, display: "flex", flexDirection: "column", textAlign:"left", alignItems: "flex-start"}}>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1200ms" }}>
+                    <ButtonBase>
+                      <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">DIGITAL ILLUSTRATIONS</Typography>
+                    </ButtonBase>
+                  </Fade>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1500ms" }}>
+                    <ButtonBase>
+                      <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">LOGO DESIGNS</Typography>
+                    </ButtonBase>
+                  </Fade>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1800ms" }}>
+                    <ButtonBase>
+                    <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">TYPOGRAPHIES</Typography>
+
+                    </ButtonBase>
+                  </Fade>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "2200ms" }}>
+                    <ButtonBase>
+                      <Typography variant="body1" letterSpacing={3} fontWeight={100} sx={{opacity: .7}} lineHeight={1.8} fontSize={18} color="background.default">PRINT DESIGNS</Typography>
+                    </ButtonBase>
+                  </Fade>
+                </Box>
+              </Grid>
+            </Slide>
+            <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "2600ms" }}>
+              <Grid item xs={8.8} sx={{zIndex: 2, display: "flex", flexDirection: "column", height: "100%", overflow: "auto", width: "100%"}}>
+                <Showcase projects={projects}/>
+              </Grid>
+            </Fade>
+          </Grid>
+        </Container>
+      </Box>
+    </Fade>
   )
 }
 
