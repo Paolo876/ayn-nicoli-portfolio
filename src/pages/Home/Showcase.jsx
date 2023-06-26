c7b8f5ddc441f16c7e36b8cd3dfefc491367484e
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-25 23:47:54 -0700

    updated showcase ui

--- src/pages/Home/Showcase.jsx
+++ src/pages/Home/Showcase.jsx
@@ -14,7 +14,7 @@ const Showcase = ({ projects, setShowModal }) => {
         indicators={false}
         navButtonsAlwaysInvisible={true}
       >
-        <Grid container sx={{display: "flex", justifyContent:"center", alignItems: 'center'}} rowSpacing={{xs: 1, sm: 3, md: 6, lg:9}} columnSpacing={0} align="center" py={2} pb={8} mb={2}>
+        <Grid container sx={{display: "flex", justifyContent:"center", alignItems: 'center'}} rowSpacing={{xs: 1, sm: 3, md: 6, lg:9}} columnSpacing={0} align="center" py={2} pb={8} mb={2} ml="auto">
           {projects.map(item => <Grid item sm={6} md={4} key={item}>
             <ButtonBase onClick={() => setShowModal({isVisible: true, src: item})}>
               <Box sx={{width: {sm: 180, md: 185, lg: 270, xl: 330}, height: {sm: 175, md: 150, lg: 240, xl:300}}}>
@@ -22,6 +22,7 @@ const Showcase = ({ projects, setShowModal }) => {
                   src={item} 
                   fit="cover" 
                   duration={1000}
+                  showLoading={true}
                 />
               </Box>
 
