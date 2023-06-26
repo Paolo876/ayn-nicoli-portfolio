82e6f069bf197db56826ff553c67e01f26ee4763
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-25 23:26:29 -0700

    updated media queries

--- src/pages/Home/Showcase.jsx
+++ src/pages/Home/Showcase.jsx
@@ -24,7 +24,8 @@ const Showcase = ({ projects, setShowModal }) => {
         <Grid container sx={{display: "flex", justifyContent:"center", alignItems: 'center'}} rowSpacing={{xs: 1, sm: 3, md: 6, lg:9}} columnSpacing={0} align="center" py={2} pb={8} mb={2}>
           {projects.map(item => <Grid item sm={6} md={4} key={item}>
             <ButtonBase onClick={() => setShowModal({isVisible: true, src: item})}>
-              <Box sx={{width: {sm: 175, md: 185, lg: 270, xl: 330}, height: {sm: 140, md: 150, lg: 240, xl:300}}}>
+              <Box sx={{width: {sm: 180, md: 185, lg: 270, xl: 330}, height: {sm: 175, md: 150, lg: 240, xl:300}}}>
+              {/* <Box sx={{}}> */}
                 <Image 
                   src={item} 
                   fit="cover" 
