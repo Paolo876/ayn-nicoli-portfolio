3487cecba5c8b996a2da5821cca3cfa355f0d3f1
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-25 23:29:22 -0700

    updated showcase component

--- src/pages/Home/Showcase.jsx
+++ src/pages/Home/Showcase.jsx
@@ -1,15 +1,8 @@
 import React from 'react'
 import Carousel from 'react-material-ui-carousel'
 import Image from 'mui-image'
-import { Paper, Button, Grid, Box, ButtonBase } from '@mui/material'
+import { Grid, Box, ButtonBase } from '@mui/material'
 
-const chunkArray = (arr, chunkSize) => {
-  return arr.reduce((all,one,i) => {
-    const ch = Math.floor(i/chunkSize); 
-    all[ch] = [].concat((all[ch]||[]),one); 
-    return all
- }, [])
-}
 
 const Showcase = ({ projects, setShowModal }) => {
 
@@ -19,13 +12,12 @@ const Showcase = ({ projects, setShowModal }) => {
         autoPlay={false}
         sx={{mb:2}}
         indicators={false}
-        navButtonsAlwaysInvisible={false}
+        navButtonsAlwaysInvisible={true}
       >
         <Grid container sx={{display: "flex", justifyContent:"center", alignItems: 'center'}} rowSpacing={{xs: 1, sm: 3, md: 6, lg:9}} columnSpacing={0} align="center" py={2} pb={8} mb={2}>
           {projects.map(item => <Grid item sm={6} md={4} key={item}>
             <ButtonBase onClick={() => setShowModal({isVisible: true, src: item})}>
               <Box sx={{width: {sm: 180, md: 185, lg: 270, xl: 330}, height: {sm: 175, md: 150, lg: 240, xl:300}}}>
-              {/* <Box sx={{}}> */}
                 <Image 
                   src={item} 
                   fit="cover" 
