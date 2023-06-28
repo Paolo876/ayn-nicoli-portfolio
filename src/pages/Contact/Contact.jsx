3c41a44cec8ce75857145f98253a0841e619cd68
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-28 00:10:36 -0700

    updated contact page ui

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,6 +6,11 @@ const Contact = () => {
   return (
     <Box sx={{display: "flex", flexDirection:"column", alignItems: "space-between", height: "100vh"}}>
       <Appbar/>
+      <Container maxWidth="xl" sx={{pt: 10}}>
+        <Grid container>
+          <Grid xs={12} align="center"><Typography variant="h3">Let's Talk</Typography></Grid>
+        </Grid>
+      </Container>
     </Box>
   )
 }
