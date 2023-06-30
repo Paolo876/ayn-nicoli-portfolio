b2ff15b7878cb4a998b4e0df329447bec48db1fa
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-29 23:45:24 -0700

    updated inquiryform ui

--- src/pages/Contact/InquiryForm.jsx
+++ src/pages/Contact/InquiryForm.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Typography, Box, TextField, FormControl, FormLabel, Button } from '@mui/material';
 
 const InquiryForm = () => {
-
+  const [ name, setName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ phone, setPhone ] = useState("");
+  const [ message, setMessage ] = useState("");
   const handleSubmit = (e) => {
     e.preventDefault();
   }
@@ -19,9 +22,6 @@ const InquiryForm = () => {
         <Typography variant="body1" lineHeight={1.25} fontWeight={200} fontSize={18}>
           For inquiries, you can reach me through my email or through submitting the form below.
         </Typography>
-        <Typography variant="body1" lineHeight={1.25} fontWeight={200} fontSize={16}>
-          I'm looking forward to hearing from you!
-        </Typography>
       </Box>
 
       <Box sx={{mb: 2.5}}>
@@ -32,6 +32,7 @@ const InquiryForm = () => {
           size="small"
           type="text"
           inputProps={{
+            maxLength: 40,
             sx: { backgroundColor: "background.default", overflow: "hidden", borderRadius: 1, opacity: .95 }
           }}
           fullWidth
@@ -45,6 +46,7 @@ const InquiryForm = () => {
           size="small"
           type="text"
           inputProps={{
+            maxLength: 50,
             sx: { backgroundColor: "background.default", overflow: "hidden", borderRadius: 1, opacity: .95 }
           }}
           fullWidth
@@ -58,6 +60,7 @@ const InquiryForm = () => {
           size="small"
           type="text"
           inputProps={{
+            maxLength: 20,
             sx: { backgroundColor: "background.default", overflow: "hidden", borderRadius: 1, opacity: .95 }
           }}
           fullWidth
@@ -71,15 +74,16 @@ const InquiryForm = () => {
           size="small"
           type="text"
           inputProps={{
-            sx: { backgroundColor: "background.default", overflow: "hidden", borderRadius: 1, opacity: .95, p:0, m:0 }
+            maxLength: 320,
+            sx: { overflow: "hidden", borderRadius: 1, p:0, m:0 }
           }}
           InputProps={{
-            sx: {p:0}
+            sx: { backgroundColor: "background.default", opacity: .95, }
           }}
           sx={{p: 0, m: 0}}
           fullWidth
           multiline
-          rows={6}
+          rows={5}
         />
       </Box>
       <Box sx={{display: "flex", justifyContent: "center", mt: 8}}>
