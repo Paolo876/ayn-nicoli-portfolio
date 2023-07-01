13fdab7e291c137b6a2e7ea2b17d256fda6891d4
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-30 23:28:03 -0700

    updated Contact page ui

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,34 +1,35 @@
 import React from 'react'
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid, Typography, Fade } from "@mui/material";
 import Appbar from '../../components/UI/Appbar';
 
 const About = () => {
   return (
-    <Box sx={{display: "flex", flexDirection:"column", alignItems: "space-between", height: "100vh"}}>
-      <Appbar/>
-      <Container maxWidth="xl" sx={{pt: 10}}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <Box sx={{width:  "100%", height: 210, backgroundColor: "rgba(0,0,0,0.2)"}}>header</Box>
-          </Grid>
-          <Grid item xs={2.5}>
-            <Box sx={{width:  "100%", height: 400, backgroundColor: "rgba(0,0,0,0.2)"}}>
-              image
-            </Box>
-          </Grid>
-          <Grid item xs={7}>
-            <Box sx={{px: 3.2}}>
-              <Typography variant="h2">Hi there!</Typography>
-              <Typography variant="body1" sx={{fontWeight: 200, lineHeight: 1.3, fontSize: 30}} mt={7}>I'm Ayn, an Architect and a Graphic Designer. I've been in the industry of designing and creating since 2017.</Typography>
-              <Typography variant="body1" sx={{fontWeight: 200, lineHeight: 1.3, fontSize: 30}} mt={3}>I am a passionate creative that has a good eye for design. For years, I've designed several types of projects, be it - Residential, Commercial, Offices...</Typography>
-            </Box>
-          </Grid>
-          <Grid item xs={2.5}></Grid>
+    <Fade appear={true} in={true} timeout={{ enter: 1000, exit: 800 }} style={{ transitionDelay: "200ms" }}>
+      <Box sx={{display: "flex", flexDirection:"column", alignItems: "space-between", height: "100vh"}}>
+        <Appbar/>
+        <Container maxWidth="xl" sx={{pt: 10}}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Box sx={{width:  "100%", height: 210, backgroundColor: "rgba(0,0,0,0.2)"}}>header</Box>
+            </Grid>
+            <Grid item xs={2.5}>
+              <Box sx={{width:  "100%", height: 400, backgroundColor: "rgba(0,0,0,0.2)"}}>
+                image
+              </Box>
+            </Grid>
+            <Grid item xs={7}>
+              <Box sx={{px: 3.2}}>
+                <Typography variant="h2">Hi there!</Typography>
+                <Typography variant="body1" sx={{fontWeight: 200, lineHeight: 1.3, fontSize: 30}} mt={7}>I'm Ayn, an Architect and a Graphic Designer. I've been in the industry of designing and creating since 2017.</Typography>
+                <Typography variant="body1" sx={{fontWeight: 200, lineHeight: 1.3, fontSize: 30}} mt={3}>I am a passionate creative that has a good eye for design. For years, I've designed several types of projects, be it - Residential, Commercial, Offices...</Typography>
+              </Box>
+            </Grid>
+            <Grid item xs={2.5}></Grid>
 
-        </Grid>
-      </Container>
-
-    </Box>
+          </Grid>
+        </Container>
+      </Box>
+    </Fade>
   )
 }
 
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, Typography, Fade, Slide } from "@mui/material";
 import logo from "../../assets/logo/ayn-logo-100-grey.png"
 import Appbar from '../../components/UI/Appbar';
 import InquiryForm from './InquiryForm';
@@ -9,47 +9,51 @@ import Image from 'mui-image';
 
 const Contact = () => {
   return (
-    <Box sx={{display: "flex", flexDirection:"column", alignItems: "space-between", height: "100vh"}}>
-      <Appbar/>
-      <Container maxWidth="xl" sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center", pt: 5 }}>
-        <Grid container sx={{ boxShadow: 20, backgroundColor: "primary.main", color: "background.default", height: "80vh"}}>
-          <Grid item xs={7} py={3} px={2} sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
-            <Box>
-              <Typography variant="h1" lineHeight={1.2} letterSpacing={.5}>Let's Work Together</Typography>
-            </Box>
-            <Box sx={{ display: "flex", gap: 3, alignItems: "center" }}>
-              <Box sx={{height: 80, width: "auto"}}>
-                <Image src={logo} fit="cover"/>
-              </Box>
-              <Box>
-                <Typography variant="h3" lineHeight={1.05} letterSpacing={.25}>Ayn Laquindanum</Typography>
-                <Typography letterSpacing={.5} variant="body2">LICENSED ARCHITECT <Box component="span">|</Box> GRAPHIC DESIGNER</Typography>
-              </Box>
-            </Box>
-            <Box ml={1}>
-              <Button 
-                href="mailto:aynslaquindanum@gmail.com" 
-                target="_blank" 
-                variant="text" 
-                color="inherit"
-                size="large"
-                startIcon={<EmailIcon/>}
-                sx={{
-                  textTransform: "none",
-                  fontWeight: 300,
-                  lineHeight: 2,
-                  letterSpacing: .15,
-                  fontSize: 25,
-                }}
-              >
-                aynslaquindanum@gmail.com
-              </Button>
-            </Box>
-          </Grid>
-          <Grid item xs={5} sx={{backgroundColor: "secondary.main"}}><InquiryForm/></Grid>
-        </Grid>
-      </Container>
-    </Box>
+      <Box sx={{display: "flex", flexDirection:"column", alignItems: "space-between", height: "100vh"}}>
+        <Appbar/>
+        {/* <Fade appear={true} in={true} timeout={{ enter: 1000, exit: 800 }} style={{ transitionDelay: "50ms" }}> */}
+        <Slide direction="down" in={true} mountOnEnter unmountOnExit timeout={800} >
+          <Container maxWidth="xl" sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center", pt: 5 }}>
+            <Grid container sx={{ boxShadow: 20, backgroundColor: "primary.main", color: "background.default", height: "80vh"}}>
+              <Grid item xs={7} py={3} px={2} sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+                <Box>
+                  <Typography variant="h1" lineHeight={1.2} letterSpacing={.5}>Let's Work Together</Typography>
+                </Box>
+                <Box sx={{ display: "flex", gap: 3, alignItems: "center" }}>
+                  <Box sx={{height: 80, width: "auto"}}>
+                    <Image src={logo} fit="cover"/>
+                  </Box>
+                  <Box>
+                    <Typography variant="h3" lineHeight={1.05} letterSpacing={.25}>Ayn Laquindanum</Typography>
+                    <Typography letterSpacing={.5} variant="body2">LICENSED ARCHITECT <Box component="span">|</Box> GRAPHIC DESIGNER</Typography>
+                  </Box>
+                </Box>
+                <Box ml={1}>
+                  <Button 
+                    href="mailto:aynslaquindanum@gmail.com" 
+                    target="_blank" 
+                    variant="text" 
+                    color="inherit"
+                    size="large"
+                    startIcon={<EmailIcon/>}
+                    sx={{
+                      textTransform: "none",
+                      fontWeight: 300,
+                      lineHeight: 2,
+                      letterSpacing: .15,
+                      fontSize: 25,
+                    }}
+                  >
+                    aynslaquindanum@gmail.com
+                  </Button>
+                </Box>
+              </Grid>
+              <Grid item xs={5} sx={{backgroundColor: "secondary.main"}}><InquiryForm/></Grid>
+            </Grid>
+          </Container>
+        {/* </Fade> */}
+        </Slide>
+      </Box>
   )
 }
 
