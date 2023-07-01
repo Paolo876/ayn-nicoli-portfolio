35d0a5956294a364582a055e177070c5fab3b05d
paoloBugarin <paolopaolobugarin@gmail.com>
2023-06-30 23:47:04 -0700

    updated Contact page ui

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -11,48 +11,55 @@ const Contact = () => {
   return (
       <Box sx={{display: "flex", flexDirection:"column", alignItems: "space-between", height: "100vh"}}>
         <Appbar/>
-        {/* <Fade appear={true} in={true} timeout={{ enter: 1000, exit: 800 }} style={{ transitionDelay: "50ms" }}> */}
-        <Slide direction="down" in={true} mountOnEnter unmountOnExit timeout={800} >
+        <Fade appear={true} in={true} timeout={{ enter: 1000, exit: 800 }} style={{ transitionDelay: "200ms" }}>
           <Container maxWidth="xl" sx={{ height: "100%", display: "flex", alignItems: "center", justifyContent: "center", pt: 5 }}>
-            <Grid container sx={{ boxShadow: 20, backgroundColor: "primary.main", color: "background.default", height: "80vh"}}>
-              <Grid item xs={7} py={3} px={2} sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
-                <Box>
-                  <Typography variant="h1" lineHeight={1.2} letterSpacing={.5}>Let's Work Together</Typography>
-                </Box>
-                <Box sx={{ display: "flex", gap: 3, alignItems: "center" }}>
-                  <Box sx={{height: 80, width: "auto"}}>
-                    <Image src={logo} fit="cover"/>
-                  </Box>
-                  <Box>
-                    <Typography variant="h3" lineHeight={1.05} letterSpacing={.25}>Ayn Laquindanum</Typography>
-                    <Typography letterSpacing={.5} variant="body2">LICENSED ARCHITECT <Box component="span">|</Box> GRAPHIC DESIGNER</Typography>
-                  </Box>
-                </Box>
-                <Box ml={1}>
-                  <Button 
-                    href="mailto:aynslaquindanum@gmail.com" 
-                    target="_blank" 
-                    variant="text" 
-                    color="inherit"
-                    size="large"
-                    startIcon={<EmailIcon/>}
-                    sx={{
-                      textTransform: "none",
-                      fontWeight: 300,
-                      lineHeight: 2,
-                      letterSpacing: .15,
-                      fontSize: 25,
-                    }}
-                  >
-                    aynslaquindanum@gmail.com
-                  </Button>
-                </Box>
+            <Slide direction="down" in={true} mountOnEnter unmountOnExit timeout={800} >
+              <Grid container sx={{ boxShadow: 20, backgroundColor: "primary.main", color: "background.default", height: "80vh"}}>
+                <Grid item xs={7} py={3} px={2} sx={{display: "flex", flexDirection: "column", justifyContent: "space-between"}}>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "700ms" }} mountOnEnter>
+                    <Box>
+                      <Typography variant="h1" lineHeight={1.2} letterSpacing={.5}>Let's Work Together</Typography>
+                    </Box>
+                  </Fade>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1000ms" }} mountOnEnter>
+                    <Box sx={{ display: "flex", gap: 3, alignItems: "center" }}>
+                      <Box sx={{height: 80, width: "auto"}}>
+                        <Image src={logo} fit="cover" duration={0}/>
+                      </Box>
+                      <Box>
+                        <Typography variant="h3" lineHeight={1.05} letterSpacing={.25}>Ayn Laquindanum</Typography>
+                        <Typography letterSpacing={.5} variant="body2">LICENSED ARCHITECT <Box component="span">|</Box> GRAPHIC DESIGNER</Typography>
+                      </Box>
+                    </Box>
+                  </Fade>
+                  <Fade appear={true} in={true} timeout={{ enter: 800, exit: 800 }} style={{ transitionDelay: "1300ms" }} mountOnEnter>
+                    <Box ml={1}>
+                      <Button 
+                        href="mailto:aynslaquindanum@gmail.com" 
+                        target="_blank" 
+                        variant="text" 
+                        color="inherit"
+                        size="large"
+                        startIcon={<EmailIcon/>}
+                        sx={{
+                          textTransform: "none",
+                          fontWeight: 300,
+                          lineHeight: 2,
+                          letterSpacing: .15,
+                          fontSize: 25,
+                        }}
+                      >
+                        aynslaquindanum@gmail.com
+                      </Button>
+                    </Box>
+                  </Fade>
+                </Grid>
+                <Grid item xs={5} sx={{backgroundColor: "secondary.main"}}><InquiryForm/></Grid>
               </Grid>
-              <Grid item xs={5} sx={{backgroundColor: "secondary.main"}}><InquiryForm/></Grid>
-            </Grid>
+            </Slide>
           </Container>
-        {/* </Fade> */}
-        </Slide>
+        </Fade>
+
       </Box>
   )
 }
