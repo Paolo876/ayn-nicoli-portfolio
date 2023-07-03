1b9757ae940a5dba2d5b142bb86bc1475c16af3d
paoloBugarin <paolopaolobugarin@gmail.com>
2023-07-02 18:06:51 -0700

    updated home page media queries

--- src/components/UI/Appbar.jsx
+++ src/components/UI/Appbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 import { Typography, AppBar, Container, Box, Link, Grid, Button } from '@mui/material';
 import { keyframes } from "@emotion/react";
 
-import logo from "../../assets/logo/ayn-logo-100.png"
+import logo from "../../assets/logo/ayn-logo-100.png";
 import Image from 'mui-image';
 
 const fadeIn = keyframes`
@@ -32,7 +32,7 @@ const linkStyles = {
 const projectLinkStyles = {
   fontWeight: 700,
   lineHeight: 1.1,
-  fontSize: {sm: 12.5, md: 13, lg: 14, xl:17},
+  fontSize: {xs: 14, sm: 12.5, md: 13, lg: 14, xl:17},
   letterSpacing: -.5,
   animation: `${fadeIn} ease-in 800ms forwards`,
 
@@ -54,7 +54,7 @@ const Appbar = ({ isArchProjActive, setIsArchProjActive }) => {
       <Container maxWidth="xl" sx={{position: "relative"}}>
         <Grid container sx={{py:{xs:.5, sm:1.15, md: 1.2, lg: .75 }, minHeight: 65}}>
           {pathname === "/" &&
-            <Grid item sm={4.5} md={4.3} lg={3.3} xl={3.2} sx={{display: "flex", justifyContent: "center", alignItems: "center", px: .5}}>
+            <Grid item xs={12} sm={4.5} md={4.3} lg={3.3} xl={3.2} sx={{display: "flex", justifyContent: "center", alignItems: "center", px: .5, pt: {xs: .5, sm: 0}, gap: {xs: 1, sm: 0}}} order={{xs: 2, sm: 1}}>
                 <Button 
                   onClick={() => setIsArchProjActive(true)}
                   sx={{...projectButtonStyles, transform: isArchProjActive ? "scale(1.05)" : "none", opacity: !isArchProjActive ? 1: .85, minWidth: 0, px: {sm:0, md: 1}}}
@@ -92,8 +92,10 @@ const Appbar = ({ isArchProjActive, setIsArchProjActive }) => {
               flexDirection: "row", 
               gap: { xs: 2.5, sm: 3, md:4, lg: 5,xl: 7.5 },
               alignItems: "center",
-              justifyContent: "right"
+              justifyContent: "right",
+              minHeight: {xs: 32, sm: "initial"}
             }}
+            order={{xs: 1, sm: 2}}
           >
             {pathname !== "/" && <Link component={NavLink} to="/" draggable={false}><Typography color="primary.main" sx={linkStyles}>PROJECTS</Typography></Link>}
             <Link component={NavLink} to="/about" draggable={false}>
@@ -126,11 +128,10 @@ const Appbar = ({ isArchProjActive, setIsArchProjActive }) => {
             </Link>
           </Grid>
         </Grid>
-        <Box sx={{position: "absolute", top: 0, left: pathname === "/" ? "50%" : 50, transform: "translateX(-50%)", p:{sm: 1, md: .5, lg: .25}}}>
+        <Box sx={{position: "absolute", top: 0, left: pathname === "/" ? {xs: 35, sm:"50%"} : 50, transform: "translateX(-50%)", p:{sm: 1, md: .5, lg: .25}}}>
           <Button component={NavLink} to="/" draggable={false} sx={{borderRadius: "50%", p:.7, minWidth: 0, minHeight: 0, overflow: "hidden" }} color="primary">
-            <Box sx={{height: {xs: 38, sm: 40, md: 45, lg:50}, width: {xs: 38, sm: 40, md: 45, lg:50}}}>
+            <Box sx={{height: {xs: 30, sm: 40, md: 45, lg:50}, width: {xs: 30, sm: 40, md: 45, lg:50}}}>
               <Image src={logo} fit="cover" duration={0}/>
-
             </Box>
           </Button>
         </Box>
--- src/pages/Home/Showcase.jsx
+++ src/pages/Home/Showcase.jsx
@@ -1,27 +1,23 @@
-import React from 'react'
-import Carousel from 'react-material-ui-carousel'
+import { useState, useEffect} from 'react'
 import Image from 'mui-image'
 import { Grid, Box, ButtonBase } from '@mui/material'
 
 
 const Showcase = ({ projects, setShowModal }) => {
-  const updatedProjects = projects.map(item => {
-    let domain = new URL(item);
-    const newUrl = `https://ik.imagekit.io/q5892cimh/tr:w-1100/${domain.pathname}${domain.search}`;
-    return newUrl
-  })
-
+  const [ updatedProjects, setUpdatedProjects ] = useState(null)
+  
+  useEffect(() => {
+    setUpdatedProjects(() => projects.map(item => {
+      let domain = new URL(item);
+      const newUrl = `https://ik.imagekit.io/q5892cimh/tr:w-1100/${domain.pathname}${domain.search}`;
+      return newUrl
+    }))
+  }, [projects])
 
   return (
-    <Box sx={{pt: 10, height: "100%", width: "100%", pl: 1, pr: .5}}>
-      <Carousel
-        autoPlay={false}
-        sx={{mb:2}}
-        indicators={false}
-        navButtonsAlwaysInvisible={true}
-      >
+    <Box sx={{pt: {sm: 10}, height: "100%", width: "100%", pl: {sm: 1, md: 3}, pr: {sm:.5, md: 2}}}>
         <Grid container sx={{display: "flex", justifyContent:"center", alignItems: 'center'}} rowSpacing={{xs: 1, sm: 3, md: 6, lg:9}} columnSpacing={{xs: 1, md:2, lg: 2, xl:3}} align="center" py={2} pb={8} mb={2} ml="auto">
-          {updatedProjects.map((item, index) => <Grid item xs={6} lg={4} key={item}>
+          {updatedProjects && updatedProjects.map((item, index) => <Grid item xs={6} lg={4} key={item}>
             <ButtonBase onClick={() => setShowModal({isVisible: true, src: projects[index]})}>
               <Box sx={{ aspectRatio: "1 / 1" }}>
                 <Image 
@@ -33,9 +29,7 @@ const Showcase = ({ projects, setShowModal }) => {
             </ButtonBase>
           </Grid>)}
         </Grid>
-      </Carousel>
     </Box>
-
   )
 }
 
